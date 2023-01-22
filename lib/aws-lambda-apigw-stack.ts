f1a0801993b31a942bcf6734edd042a0d5cbf3d9
zainuleb <zainulebadd@gmail.com>
2023-01-23 03:33:00 +0500

    completed apigw integration with lambda

--- lib/aws-lambda-apigw-stack.ts
+++ lib/aws-lambda-apigw-stack.ts
@@ -1,15 +1,18 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class AwsLambdaApigwStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, 'Salam Foo', {
+    const salam = new lambda.Function(this, 'Salam Foo', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler',
+      handler: 'salam.handler',
     });
+
+    const api = new apigw.LambdaRestApi(this, 'Endpoint', { handler: salam });
   }
 }
